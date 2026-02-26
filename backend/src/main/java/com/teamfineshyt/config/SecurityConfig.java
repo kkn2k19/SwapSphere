dc0bcc297fdd2b6fa6406adefd0697078ef0ba05
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2026-02-26 12:03:28 +0530

    .

--- backend/src/main/java/com/teamfineshyt/config/SecurityConfig.java
+++ backend/src/main/java/com/teamfineshyt/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.teamfineshyt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,12 +25,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> {})
+                .cors(cors -> {
+                })
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+
+                        // Allow preflight (imp for react )
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                        // auth apis
                         .requestMatchers("/api/auth/**").permitAll()
-            
+
+                        // product apis
+                        .requestMatchers(HttpMethod.GET, "/api/products").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/*").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/productConditions").permitAll()
+
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
