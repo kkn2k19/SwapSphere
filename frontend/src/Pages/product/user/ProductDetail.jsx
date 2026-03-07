58cbad30374b93f5f3083011572a188f6c4f4467
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2026-03-07 22:39:53 +0530

    .

--- frontend/src/Pages/product/user/ProductDetail.jsx
+++ frontend/src/Pages/product/user/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import api from '../../services/api'
+import api from '../../../services/api'
 import { jwtDecode } from "jwt-decode";
 
 const ProductDetail = () => {
---
+++ frontend/src/Pages/product/user/admin/AdminExchanges.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminExchanges = () => {
+    return (
+        <div>AdminExchanges</div>
+    )
+}
+
+export default AdminExchanges
