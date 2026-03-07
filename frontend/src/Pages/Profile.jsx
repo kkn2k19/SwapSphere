4fb15bbf83877d04818ac6d9090860ca1c78b0ca
Karan Kumar Nonia <karankumarnonia11@gmail.com>
2026-03-07 22:22:33 +0530

    .

--- frontend/src/Pages/Profile.jsx
+++ frontend/src/Pages/Profile.jsx
@@ -4,6 +4,7 @@ import api from '../services/api'
 const Profile = () => {
     const [profile, setProfile] = useState(null);
     const [editMode, setEditMode] = useState(false);
+    const [passChangeMode, setPassChangeMode] = useState(false);
 
     const [form, setForm] = useState({
         name: "",
@@ -31,117 +32,191 @@ const Profile = () => {
                     pincode: res.data.pincode
                 })
             })
+            .catch(() => alert("Failed to load profile"))
     }, [])
 
     const updateProfile = async () => {
-        await api.put("/api/auth/update-profile", form)
-        alert("Profile updated successfully")
-        setEditMode(false)
+        try {
+            await api.put("/api/auth/update-profile", form)
+            alert("Profile updated successfully")
+            // setEditMode(false)
+
+            setProfile({
+                ...profile,
+                ...form
+            })
+            setEditMode(false)
+        } catch {
+            alert("Failed to update profile")
+        }
     }
 
     const changePassword = async () => {
-        await api.put("/api/auth/change-password", passwordForm)
-        alert("Password changed successfully")
-        setPasswordForm({
-            oldPassword: "",
-            newPassword: "",
-            confirmPassword: ""
-        })
+        try {
+            await api.put("/api/auth/change-password", passwordForm)
+            alert("Password changed successfully")
+            setPasswordForm({
+                oldPassword: "",
+                newPassword: "",
+                confirmPassword: ""
+            })
+            setPassChangeMode(false)
+        } catch {
+            alert("Password change failed")
+        }
     }
 
     if (!profile) return <div className='p-8'>Loading...</div>
 
     return (
         // <div>Profile</div>
-        <div className='max-w-3xl mx-auto '>
-            <h1 className='text-2xl font-bold mb-6'>My Profile</h1>
-            <div className='bg-white shadow p-6 rounded space-y-4'>
-                <p > <strong>Email:</strong>{profile.email}</p>
-                <p><strong>Role:</strong>{profile.role}</p>
-
-                <input
-                    disabled={!editMode}
-                    value={form.name}
-                    onChange={e => setForm({
-                        ...form, name: e.target.value
-                    })}
-                    className='w-full border p-2'
-                />
-
-                <input
-                    disabled={!editMode}
-                    value={form.phone}
-                    onChange={e => setForm({
-                        ...form, phone: e.target.value
-                    })}
-                />
-                <input
-                    disabled={!editMode}
-                    value={form.city}
-                    onChange={e => setForm({
-                        ...form, city: e.target.value
-                    })}
-                    className='w-full border p-2'
-                />
-
-                <input
-                    disabled={!editMode}
-                    value={form.state}
-                    onChange={e => setForm({
-                        ...form, state: e.target.value
-                    })}
-                />
-                <input
-                    disabled={!editMode}
-                    value={form.pincode}
-                    onChange={e => setForm({
-                        ...form, pincode: e.target.value
-                    })}
-                />
+        <div className='min-h-screen bg-gray-100 p-6' >
+            <div className='max-w-xl mx-auto bg-white rounded-xl shadow p-6'>
+                <h1 className='text-2xl font-bold mb-4'>My Profile</h1>
 
                 {!editMode ? (
-                    <button onClick={() => setEditMode(true)} className='bg-orange-600 text-white px-4 py-2 rounded'>
-                        Edit Profile
-                    </button>
+                    <>
+                        <div className='space-y-2 text-sm'>
+                            <p><b>Name:</b> {profile.name}</p>
+                            <p><b>Email:</b> {profile.email}</p>
+                            <p><b>Phone:</b> {profile.phone}</p>
+                            <p><b>Role:</b> {profile.role}</p>
+                            <p><b>City:</b> {profile.city}</p>
+                            <p><b>State:</b> {profile.state}</p>
+                            <p><b>Pincode:</b> {profile.pincode}</p>
+                        </div>
+                        <div className='mt-4 flex gap-3'>
+                            <button
+                                onClick={() => {
+                                    setEditMode(true)
+                                    setPassChangeMode(false)
+                                }}
+                                className='bg-blue-500 text-white px-4 py-2 rounded'
+                            >
+                                Edit Profile
+                            </button>
+                            <button onClick={() => {
+                                setPassChangeMode(true)
+                                setEditMode(false)
+                            }}
+                                className='bg-orange-500 text-white px-4 py-2 rounded'
+                            >
+                                Change Password
+                            </button>
+                        </div>
+                    </>
                 ) : (
-                    <button onClick={updateProfile} className='bg-green-600 text-white px-4 py-2 rounded'>
-                        Save Changes
-                    </button>
+                    <div className='space-y-3 text-sm'>
+                        <input
+                            value={form.name}
+                            onChange={e => setForm({
+                                ...form,
+                                name: e.target.value
+                            })}
+                            placeholder='Name'
+                            className='w-full border p-2 rounded'
+                        />
+                        <input
+                            value={form.phone}
+                            onChange={e => setForm({
+                                ...form,
+                                phone: e.target.value
+                            })}
+                            placeholder='Phone'
+                            className='w-full border p-2 rounded'
+                        />
+                        <input
+                            value={form.city}
+                            onChange={e => setForm({
+                                ...form,
+                                city: e.target.value
+                            })}
+                            placeholder='City'
+                            className='w-full border p-2 rounded'
+                        />
+                        <input
+                            value={form.state}
+                            onChange={e => setForm({
+                                ...form,
+                                state: e.target.value
+                            })}
+                            placeholder='State'
+                            className='w-full border p-2 rounded'
+                        />
+                        <input
+                            value={form.pincode}
+                            onChange={e => setForm({
+                                ...form,
+                                pincode: e.target.value
+                            })}
+                            placeholder='Pincode'
+                            className='w-full border p-2 rounded'
+                        />
+
+                        <div className='flex gap-3'>
+                            <button
+                                onClick={updateProfile}
+                                className='bg-green-500 text-white px-4 py-2 rounded'>
+                                Save Changes
+                            </button>
+                            <button
+                                onClick={() => setEditMode(false)}
+                                className='bg-gray-400 text-white px-4 py-2 rounded'>
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
                 )}
             </div>
 
-            <div className='bg-white shadow p-6 rounded space-y-3'>
-                <h2 className='text-lg font-semibold ' >Change Password</h2>
+            {passChangeMode && (
+                <div className='max-w-xl mx-auto bg-white rounded-xl shadow p-6 mt-6'>
+                    <h2 className='text-lg font-semibold mb-3' >Change Password</h2>
 
-                <input type="password"
-                    placeholder='Old Password'
-                    value={passwordForm.oldPassword}
-                    onChange={e => setPasswordForm({
-                        ...passwordForm, oldPassword: e.target.value
-                    })}
-                    className='w-full border p-2'
-                />
-                <input type="password"
-                    placeholder='New Password'
-                    value={passwordForm.newPassword}
-                    onChange={e => setPasswordForm({
-                        ...passwordForm, newPassword: e.target.value
-                    })}
-                    className='w-full border p-2'
-                />
-                <input type="password"
-                    placeholder='Confirm Password'
-                    value={passwordForm.confirmPassword}
-                    onChange={e => setPasswordForm({
-                        ...passwordForm, confirmPassword: e.target.value
-                    })}
-                    className='w-full border p-2'
-                />
-                <button onClick={changePassword}
-                    className='bg-red-600 text-white px-4 py-2 rounded'>
-                    Change Password
-                </button>
-            </div>
+                    <div className='space-y-3'>
+                        <input type="password"
+                            placeholder='Old Password'
+                            value={passwordForm.oldPassword}
+                            onChange={e => setPasswordForm({
+                                ...passwordForm,
+                                oldPassword: e.target.value
+                            })}
+                            className='w-full border p-2 rounded'
+                        />
+                        <input type="password"
+                            placeholder='New Password'
+                            value={passwordForm.newPassword}
+                            onChange={e => setPasswordForm({
+                                ...passwordForm,
+                                newPassword: e.target.value
+                            })}
+                            className='w-full border p-2 rounded'
+                        />
+                        <input type="password"
+                            placeholder='Confirm Password'
+                            value={passwordForm.confirmPassword}
+                            onChange={e => setPasswordForm({
+                                ...passwordForm,
+                                confirmPassword: e.target.value
+                            })}
+                            className='w-full border p-2 rounded'
+                        />
+                        <div className='flex gap-3'>
+                            <button onClick={changePassword}
+                                className='bg-red-600 text-white px-4 py-2 rounded'
+                            >
+                                Change Password
+                            </button>
+                            <button
+                                onClick={() => setPassChangeMode(false)}
+                                className='bg-gray-400 text-white px-4 py-2 rounded'
+                            >
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
